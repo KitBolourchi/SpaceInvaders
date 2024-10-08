de1dd24065f08eb3a6c3ff0bd87c5ca1bc97b0fe
kitbolourchi <74569356+KitBolourchi@users.noreply.github.com>
2024-10-08 19:17:30 +0100

    Update the into screen

--- composeApp/src/desktopMain/kotlin/App.kt
+++ composeApp/src/desktopMain/kotlin/App.kt
@@ -1,8 +1,5 @@
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -11,14 +8,12 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import spaceinvaders.composeapp.generated.resources.Res
 import spaceinvaders.composeapp.generated.resources.Space_Invaders_Logo
-import spaceinvaders.composeapp.generated.resources.compose_multiplatform
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
@@ -26,19 +21,20 @@ import spaceinvaders.composeapp.generated.resources.compose_multiplatform
 fun App() {
     MaterialTheme {
         Box(Modifier.fillMaxSize().background(Color.Black))
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("PLAY")
-            }
+        var showContent by remember { mutableStateOf(true) }
+        if (showContent) {
             AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
                     Image(
                         painterResource(Res.drawable.Space_Invaders_Logo),
                         null,
                     )
-                    Text(greeting)
+
+                    Column(Modifier.fillMaxWidth(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                        Button(onClick = { showContent = !showContent }) {
+                            Text("PLAY")
+                        }
+                    }
                 }
             }
         }
